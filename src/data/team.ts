c07c0a124bbb9626784bffff9b5380233971be1e
Liz Wait <ELIZABETH.M.WAIT@GMAIL.COM>
2023-12-03 14:00:54 -0600

    Revert "Beth's updates 12/1"

--- src/data/team.ts
+++ src/data/team.ts
@@ -20,7 +20,7 @@ export const TEAM: Member[] = [{
 {
     name: 'Christine Dillon',
     title: 'Mentor Director',
-    img: 'assets/images/team/Christine.jpeg'
+    img: 'assets/images/team/Placeholder_Image.jpg'
 }, {
     name: 'Beth',
     title: 'Volunteer Director',
@@ -29,5 +29,5 @@ export const TEAM: Member[] = [{
 {
     name: 'Kit Pyles',
     title: 'Attendee Director',
-    img: 'src/assets/images/team/kit.jpg'
+    img: 'assets/images/team/Placeholder_Image.jpg'
 },];
